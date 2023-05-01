7056e8a3f06952041d8d357318dff02c80091160
Jade Nguyen <jade.vu.nguyen@gmail.com>
2023-05-01 14:29:24 -0700

    Adding logic

--- server.py
+++ server.py
@@ -3,3 +3,10 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 class RequestHandler(BaseHTTPRequestHandler):
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])
+        body = self.rfile.read(content_length).decode('utf-8')
+        print("Received message: ", body)
+        self.send_response(200)
+        self.end_headers()
+server_address = ('localhost', 8000)
+httpd = HTTPServer(server_address, RequestHandler)
+httpd.serve_forever()
